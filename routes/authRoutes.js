713bf033e883c158e1ad52ec6164da36ce1f8f01
David Yim <yimd85@gmail.com>
2018-08-05 13:53:04 -0400

    updated authroutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,10 @@
 const passport = require('passport');
 
 module.exports = app => {
-  app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
-    })
-  );
+  app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-  app.get(
-    '/auth/google/callback',
-    passport.authenticate('google'),
-    (req, res) => {
-      res.redirect('/blogs');
-    }
+  app.get('/auth/google/callback', passport.authenticate('google'),
+    (req, res) => { res.redirect('/blogs'); }
   );
 
   app.get('/auth/logout', (req, res) => {
