05564069790b8e80b872179555a7da35049d30a1
David Yim <yimd85@gmail.com>
2018-08-28 17:11:58 -0400

    travis build

--- services/cache.js
+++ services/cache.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
-
 const keys = require('../config/keys');
 
 // const redisUrl = 'redis://127.0.0.1:6379';
