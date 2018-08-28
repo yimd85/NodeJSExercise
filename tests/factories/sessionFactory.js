9fe082158aee9b7a067a06b767be808ae3af6086
David Yim <yimd85@gmail.com>
2018-08-28 17:27:34 -0400

    updated build

--- tests/factories/sessionFactory.js
+++ tests/factories/sessionFactory.js
@@ -1,6 +1,7 @@
 const Buffer = require('safe-buffer').Buffer;
 const Keygrip = require('keygrip');
 const keys = require('../../config/keys');
+
 const keygrip = new Keygrip([keys.cookieKey]);
 
 module.exports = user => {
