70a79b1805590352452b86e09b69a172542ab04d
David Yim <yimd85@gmail.com>
2018-08-29 22:17:10 -0400

    added AWS sdk, aws routes

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
     googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
     mongoURI: 'mongodb://yimd85:password1@ds111562.mlab.com:11562/blog_dev',
     cookieKey: '123123123',
-    redisUrl: 'redis://127.0.0.1:6379'
+    redisUrl: 'redis://127.0.0.1:6379',
+    accessKeyId: 'AKIAIAJY4M4SYHZJ6JJA',
+    secretAccessKey: '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',
 };
